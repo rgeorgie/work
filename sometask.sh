20d2d0c537e3558f87398b396d4e7a9198cb6eb3
rgeorgiev <rosen.halifax@gmail.com>
2019-08-22 17:10:14 +0300

    Revert "Commited from PyCharm"
    This reverts commit aeb7dbc3ec972e1a8ebc43e3e87723f4dbe22cb7.

--- sometask.sh
+++ sometask.sh
@@ -1,6 +1,4 @@
 #! /bin/bash
 
 echo "This is the task to be executed" &&
-echo "This task is executed at `date` by $USER" >> /log/crontasktest.log &&
-
-sleep 10
+echo "This task is executed at `date` by $USER" >> /home/rgeorgiev/IBM/work/crontasktest.log
